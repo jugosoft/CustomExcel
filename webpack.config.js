c0c192f822942a126d94ff9da73cfc75fd870b02
jugosoft <sardimka@gmail.com>
2021-09-07 22:35:18 +0400

    base setup webpack finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,7 +9,31 @@ module.exports = {
     mode: 'development', //default mode btw
     entry: './main.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:base64].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core'),
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin.CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/image/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash:base64].css'
+        })
+    ]
 };
