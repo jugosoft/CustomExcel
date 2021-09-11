0bf5e40cad6bb3b5c0cd708346cc0dd0f75095b5
jugosoft <sardimka@gmail.com>
2021-09-11 19:08:52 +0400

    Fuck the ESLint

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import './scss/main.scss';
-
+import Excel from './components/excel/excel';
+import Header from './components/header/header';
+import Toolbar from './components/toolbar/toolbar';
+import Formula from './components/formula/formula';
+import Table from './components/table/table';
 // Надоело глазеть на стандартный вывод
 console.clear();
 
 // Всё равно не помогло
+const excelApp = new Excel('#app', {
+    components: [
+        Header,
+        Toolbar,
+        Formula,
+        Table
+    ]
+});
+
+excelApp.display();
+
